f86a67f7d700f73da53ab87181d8c309638a8aeb
julian-salinas <juliansalinasm@gmail.com>
2024-05-29 14:45:04 -0300

    Populo tabla de ubicaciones

--- sql/migracion/supermercado.sql
+++ sql/migracion/supermercado.sql
@@ -26,4 +26,18 @@ FROM (
 ) localidades
 INNER JOIN EL_DROPEO.Provincia p ON localidades.provincia = p.nombre;
 
+INSERT INTO EL_DROPEO.Ubicacion (calle, altura, localidad_id)
+SELECT 
+    LEFT(direccion, LEN(direccion) - CHARINDEX(' ', REVERSE(direccion))) AS calle,
+    CAST(RIGHT(direccion, CHARINDEX(' ', REVERSE(direccion)) - 1) AS INT) AS altura,
+    l.id AS localidad_id
+FROM (
+    SELECT CLIENTE_DOMICILIO AS direccion, CLIENTE_LOCALIDAD AS localidad FROM gd_esquema.Maestra WHERE CLIENTE_DOMICILIO IS NOT NULL
+    UNION
+    SELECT SUCURSAL_DIRECCION AS direccion, SUCURSAL_LOCALIDAD AS localidad FROM gd_esquema.Maestra WHERE SUCURSAL_DIRECCION IS NOT NULL
+    UNION
+    SELECT SUPER_DOMICILIO AS direccion, SUPER_LOCALIDAD AS localidad FROM gd_esquema.Maestra WHERE SUPER_DOMICILIO IS NOT NULL
+) direcciones
+INNER JOIN EL_DROPEO.Localidad l ON direcciones.localidad = l.nombre;
+
 END
